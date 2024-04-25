4678ee1781bcf78c8d97b10192dc01b9d1b35d12
StefE14 <163153721+StefE14@users.noreply.github.com>
2024-04-25 08:16:23 -0300

    Clase 2

---
+++ lib/core/router.dart
@@ -0,0 +1,18 @@
+import 'package:clase_2/screens/home_screen.dart';
+import 'package:clase_2/screens/login_screen.dart';
+import 'package:go_router/go_router.dart';
+
+final appRouter = GoRouter(
+  routes: [
+    GoRoute(
+      name: LoginScreen.name,
+      path: '/',
+      builder: (context, state) => const LoginScreen(),
+    ),
+    GoRoute(
+      name: HomeScreen.name,
+      path: '/home',
+      builder: (context, state) => HomeScreen(userName: state.extra as String),
+    ),
+  ],
+);
--- lib/main.dart
+++ lib/main.dart
@@ -1,78 +1,18 @@
+import 'package:clase_2/core/router.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MainApp());
+  runApp(const MainApp());
 }
 
-class MainApp extends StatefulWidget {
-  MainApp({super.key});
-
-  @override
-  State<MainApp> createState() => _MainAppState();
-}
-
-class _MainAppState extends State<MainApp> {
-  TextEditingController userController = TextEditingController();
-
-  TextEditingController passController = TextEditingController();
-  bool hidePass = true;
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Column(
-                children: [
-                  TextField(
-                    decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'User',
-                        prefixIcon: Icon(Icons.person)),
-                    controller: userController,
-                  ),
-                  TextField(
-                    obscureText: hidePass,
-                    decoration: InputDecoration(
-                      border: const OutlineInputBorder(),
-                      labelText: 'Password',
-                      prefixIcon: const Icon(Icons.key),
-                      suffixIcon: IconButton(
-                        icon: hidePass
-                            ? const Icon(Icons.visibility_off)
-                            : const Icon(Icons.visibility),
-                        onPressed: togglePasswordVisibility,
-                      ),
-                    ),
-                    controller: passController,
-                  ),
-                ],
-              ),
-              ElevatedButton(
-                child: const Text('Login'),
-                onPressed: () {
-                  String user = userController.text;
-                  String pass = passController.text;
-                  if (user.isEmpty || pass.isEmpty) {
-                    print('User or password not filled');
-                  } else {
-                    if (user == 'Jorge' && pass == 'Jorge123') {
-                      print('Login Exitoso');
-                    } else {
-                      print('Login Fallido');
-                    }
-                  }
-                },
-              )
-            ],
-          ),
-        ),
-      ),
+    return MaterialApp.router(
+      routerConfig: appRouter,
+      debugShowCheckedModeBanner: false,
     );
   }
-
-  void togglePasswordVisibility() => setState(() => hidePass = !hidePass);
 }
